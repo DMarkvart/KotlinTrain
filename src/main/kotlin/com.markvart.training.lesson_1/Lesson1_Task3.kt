c008501b9cea86f43055fdd76ebb4eb6b9a074c6
Dmitry Markvart <mdmarkwart@gmail.com>
2023-09-12 23:03:12 +0300

    Add variables year, hour, minute Init and output variables

--- src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task3.kt
+++ src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task3.kt
@@ -1,2 +1,16 @@
 package com.markvart.training.lesson_1
 
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(String.format("Год полета - %d", year))
+    println(String.format("Взлет - %02d:%02d", hour, minute))
+
+    hour = 10
+    minute = 55
+
+    println(String.format("Посадка - %02d:%02d", hour, minute))
+
+}
