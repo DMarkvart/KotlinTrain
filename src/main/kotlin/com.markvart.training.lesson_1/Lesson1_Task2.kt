2b2aa78ae6c7a6e2f3ef2a2b7b5744d02f139c31
Dmitry Markvart <mdmarkwart@gmail.com>
2023-09-12 22:43:54 +0300

    KS-1-2 add output in console variables countOfOrders and gratitudeByShopping add variable countOfEmployes and output in console

--- src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task2.kt
+++ src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task2.kt
@@ -1,2 +1,12 @@
 package com.markvart.training.lesson_1
 
+fun main(args: Array<String>) {
+    val countOfOrders = 75
+    val gratitudeByShopping = "Thanks for the purchase!"
+    println(countOfOrders)
+    println(gratitudeByShopping)
+    var countOfEmployes = 2000
+//    println(countOfEmployes)
+    countOfEmployes = 1999
+    println(countOfEmployes)
+}
