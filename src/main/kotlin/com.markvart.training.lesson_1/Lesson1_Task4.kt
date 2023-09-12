d726d0718690671e017ac852ab528ef889f32dd1
Dmitry Markvart <mdmarkwart@gmail.com>
2023-09-12 23:20:51 +0300

    Add task variables and output in console

--- src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task4.kt
+++ src/main/kotlin/com.markvart.training.lesson_1/Lesson1_Task4.kt
@@ -1,2 +1,17 @@
 package com.markvart.training.lesson_1
 
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partyOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Long = 327000
+
+    println(length)
+    println(age)
+    println(partyOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
